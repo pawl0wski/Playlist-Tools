40a297f4de56fe5cc42deff7475af0eca560dd52
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-01-22 15:44:48 +0100

    Show burger menu if user click on NavigationBurgerBtn

--- src/components/NavigationBar/NavigationBar.vue
+++ src/components/NavigationBar/NavigationBar.vue
@@ -1,17 +1,23 @@
 <template>
   <nav>
-      <router-link to="/">
-          <h3>{{name}}</h3>
-      </router-link>
-      <NavigationContent v-if="!burgerMode"></NavigationContent>
-      <NavigationUser :avatarUrl="avatarUrl" :username="username" v-if="isAuthorized && !burgerMode" @logOut="logOut"></NavigationUser>
+    <router-link to="/">
+        <h3>{{name}}</h3>
+    </router-link>
+    <NavigationContent v-if="!burgerMode"></NavigationContent>
+    <NavigationUser :avatarUrl="avatarUrl" :username="username" v-if="isAuthorized && !burgerMode" @logOut="logOut"></NavigationUser>
+    <NavigationBurgerBtn v-if="burgerMode" @click="toggleBurgerMenu"></NavigationBurgerBtn>
   </nav>
+  <div class="burger-menu" :style="{top: burgerMenuShowed ? '60px' : '-70px'}" v-if="burgerMode">
+    <NavigationContent></NavigationContent>
+    <NavigationUser :avatarUrl="avatarUrl" :username="username" v-if="isAuthorized" @logOut="logOut"></NavigationUser>
+  </div>
 </template>
 
 <script lang="ts">
 import { SpotifyCreator } from "@/libs/spotify_connector/spotify_creator";
 import NavigationUser from "@/components/NavigationBar/NavigationUser.vue";
 import NavigationContent from "@/components/NavigationBar/NavigationContent.vue";
+import NavigationBurgerBtn from "@/components/NavigationBar/NavigationBurgerBtn.vue";
 import { defineComponent } from "vue";
 export default defineComponent ({
     data() {
@@ -21,11 +27,13 @@ export default defineComponent ({
             username: "",
             isAuthorized: false,
             burgerMode: false,
+            burgerMenuShowed: false,
         }
     },
     components: {
         NavigationUser,
-        NavigationContent
+        NavigationContent,
+        NavigationBurgerBtn
     },
     methods: {
         logOut() {
@@ -48,15 +56,18 @@ export default defineComponent ({
                     this.$data.username = username;
                 });
             }
+        },
+        updateBurgerMode() {
+            this.$data.burgerMode = window.innerWidth < 570;
+        },
+        toggleBurgerMenu(){
+            this.$data.burgerMenuShowed = !this.$data.burgerMenuShowed;
         }
     },
     beforeMount() {
         this.updateSpotifyData()
-        window.addEventListener("resize", () => {
-            console.log("Resize!")
-            this.$data.burgerMode = window.innerWidth < 570;
-            console.log(this.$data.burgerMode)
-        })
+        this.updateBurgerMode()
+        window.addEventListener("resize", this.updateBurgerMode)
     },
 })
 </script>
@@ -64,6 +75,7 @@ export default defineComponent ({
 <style lang="scss" scoped>
     nav{
         position: sticky;
+        z-index: 50;
         top: 0;
         height: 60px;
         background-color: darken($color: $bg-color, $amount: 0.2);
@@ -81,6 +93,28 @@ export default defineComponent ({
             h3{
                 margin: 0;
             }
+        }
+    }
+
+    div.burger-menu {
+        position: fixed;
+        top: -100%;
+        transition: 0.25s top;
+        width: 100vw;
+        background-color: $bg-color;
+
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+
+        gap: 1em;
+
+        padding-bottom: 1em;
+
+        div.user-info {
+            margin: unset;
         }
     }
 </style>
---
+++ src/components/NavigationBar/NavigationBurgerBtn.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="navigation-burger-button">
+        <i class="fas fa-bars"></i>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    div.navigation-burger-button{
+        margin-left: auto;
+        border-width: 2px;
+        border-style: solid;
+        border-color: lighten($color: $content-color, $amount: 0.5);
+        padding: 5px 10px;
+        border-radius: 10px;
+        cursor: pointer;
+    }
+</style>
