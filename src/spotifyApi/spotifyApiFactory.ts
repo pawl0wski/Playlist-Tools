3e5461d7bbfc48f1cb56c2da012f7e861ffb032f
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-02-13 19:23:43 +0100

    Change scopes

--- src/spotifyApi/spotifyApiFactory.ts
+++ src/spotifyApi/spotifyApiFactory.ts
@@ -6,14 +6,14 @@ export class SpotifyApiFactory {
         return SpotifyApi.getInstance(
             process.env.VUE_APP_SPOTIFY_KEY,
             `http://${process.env.VUE_APP_REDIRECT_DOMAIN}/authorize`,
-            "playlist-modify-private user-library-read user-read-private playlist-modify-public user-library-modify playlist-read-collaborative playlist-read-private "
+            "playlist-modify-private playlist-read-collaborative user-read-private playlist-modify-public playlist-read-private"
         );
     }
     static createCachedSpotifyApiWithDefaultApp(): CachedSpotifyApi {
         return CachedSpotifyApi.getInstance(
             process.env.VUE_APP_SPOTIFY_KEY,
             `http://${process.env.VUE_APP_REDIRECT_DOMAIN}/authorize`,
-            "playlist-modify-private user-library-read user-read-private playlist-modify-public user-library-modify playlist-read-collaborative"
+            "playlist-modify-private playlist-read-collaborative user-read-private playlist-modify-public playlist-read-private"
         );
     }
 }
